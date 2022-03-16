fd37c8e644f1c28543faa6cbf3917832fc368db9
EdwardSPO <edwardp@nserio.com>
2022-03-16 16:58:07 -0500

    init

---
+++ translator_front/tests/components/Dashboard.spec.js
@@ -0,0 +1,5 @@
+describe('Dashboard.vue', ()=>{
+    test('setup correctly', () =>{
+   expect(true).toBe(true);
+    });
+  });
---
+++ translator_front/tests/components/Editar.spec.js
@@ -0,0 +1,5 @@
+describe('Nuevo.vue', ()=>{
+  test('setup correctly', () =>{
+ expect(true).toBe(true);
+  });
+});
